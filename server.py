499f4089d1b5c2180a9e9f42db85d309b47e6f8d
Rostislav <rostiabrslav@gmail.com>
2020-03-05 02:28:01 +1000

    added some Keyboard to command /expenses

--- server.py
+++ server.py
@@ -1,15 +1,27 @@
-from aiogram import Dispatcher, Bot, types
+from aiogram import Dispatcher, Bot, executor
 from aiogram.bot import api
 from config import TOKEN_API
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, Message
+import logging
+
 
 dp = Dispatcher(bot=Bot(token=TOKEN_API))
+logging.basicConfig(level=logging.INFO)
+
+
+buttons = [[KeyboardButton(text='day'),
+           KeyboardButton(text='month'),
+           KeyboardButton(text='year')]]
+
+period_markup = ReplyKeyboardMarkup(buttons, resize_keyboard=True, one_time_keyboard=True)
+
 
 """Начало общения с ботом"""
 
 
 @dp.message_handler(commands=['start', 'help'])
-async def send_help(message: types.Message):
-    await message.reply(
+async def send_start(message: Message):
+    await message.answer(
         text='Я телеграм бот для подсчета расходов'
     )
 
@@ -18,7 +30,7 @@ async def send_help(message: types.Message):
 
 
 @dp.message_handler(commands=['categories'])
-async def send_categories(message: types.Message):
+async def send_categories(message: Message):
     pass
 
 
@@ -26,15 +38,15 @@ async def send_categories(message: types.Message):
 
 
 @dp.message_handler(commands=['expenses'])
-async def send_expenses(message: types.Message):
-    pass
+async def send_expenses(message: Message):
+    await message.answer('Выберите период за который показать расходы: ', reply_markup=period_markup)
 
 
 """Вывод списка доходов"""
 
 
 @dp.message_handler(commands=['revenues'])
-async def send_revenues(message: types.Message):
+async def send_revenues(message: Message):
     pass
 
 
@@ -42,7 +54,7 @@ async def send_revenues(message: types.Message):
 
 
 @dp.message_handler(commands=['savings'])
-async def send_savings(message: types.Message):
+async def send_savings(message: Message):
     pass
 
 
@@ -50,5 +62,14 @@ async def send_savings(message: types.Message):
 
 
 @dp.message_handler(commands=['statistics'])
-async def send_revenues(message: types.Message):
+async def send_revenues(message: Message):
     pass
+
+
+@dp.message_handler(lambda message: 'day' == message.text)
+async def send_message(message: Message):
+    await message.answer('test')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
